d62e626bf5f7b38348a630ff7362a11539ee8c67
Rodrigo Vieira <rodrigo@vieira.com>
2018-06-05 19:29:44 -0300

    [Fix-2] DAnchor <a> colors :/ - Removed data-scroll property

--- src/components/DAnchor/index.vue
+++ src/components/DAnchor/index.vue
@@ -4,7 +4,7 @@
     :nodes="nodes" node-key="id"
     :selected.sync="selected" ref="anchor">
     <div slot="default-header" slot-scope="prop" class="row items-center">
-      <a v-if="prop.node.h" data-scroll :href="`/${route}#${prop.node.h[0]}`">{{ prop.node.h[1] }}</a>
+      <a v-if="prop.node.h" :href="`/${route}#${prop.node.h[0]}`">{{ prop.node.h[1] }}</a>
     </div>
   </q-tree>
 </template>
