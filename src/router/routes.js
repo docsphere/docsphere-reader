2ff3406075a4c42ecfd3c8c776a14f7a15a01c91
Rodrigo Vieira <rodrigo@vieira.com>
2018-06-12 13:09:03 -0300

    [Update] Routes - Added some routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -25,6 +25,136 @@ export default [
       }
     ]
   },
+  {
+    path: '/starting/platform',
+    component: () => import('layouts/default'),
+    meta: {
+      dir: '0-starting/2-platform',
+      icon: 'phonelink',
+
+      layouts: {
+        footer: true,
+        submenu: true
+      },
+      pages: {
+        showcase: false
+      }
+    },
+
+    children: [
+      {
+        path: '',
+        component: () => import('pages/0-starting/2-platform/'),
+        meta: {
+          status: 0
+        }
+      }
+    ]
+  },
+  {
+    path: '/starting/events',
+    component: () => import('layouts/default'),
+    meta: {
+      dir: '0-starting/3-events',
+      icon: 'event',
+
+      layouts: {
+        footer: true,
+        submenu: true
+      },
+      pages: {
+        showcase: false
+      }
+    },
+
+    children: [
+      {
+        path: '',
+        component: () => import('pages/0-starting/3-events/'),
+        meta: {
+          status: 0
+        }
+      }
+    ]
+  },
+  {
+    path: '/starting/injections',
+    component: () => import('layouts/default'),
+    meta: {
+      dir: '0-starting/4-injections',
+      icon: 'vertical_align_bottom',
+
+      layouts: {
+        footer: true,
+        submenu: true
+      },
+      pages: {
+        showcase: false
+      }
+    },
+
+    children: [
+      {
+        path: '',
+        component: () => import('pages/0-starting/4-injections/'),
+        meta: {
+          status: 0
+        }
+      }
+    ]
+  },
+  {
+    path: '/starting/i18n',
+    component: () => import('layouts/default'),
+    meta: {
+      dir: '0-starting/5-i18n',
+      icon: 'language',
+
+      layouts: {
+        footer: true,
+        submenu: true
+      },
+      pages: {
+        showcase: false
+      }
+    },
+
+    children: [
+      {
+        path: '',
+        component: () => import('pages/0-starting/5-i18n/'),
+        meta: {
+          status: 0
+        }
+      }
+    ]
+  },
+  {
+    path: '/starting/rtl',
+    component: () => import('layouts/default'),
+    meta: {
+      dir: '0-starting/6-rtl',
+      icon: 'format_list_numbered_rtl',
+
+      layouts: {
+        footer: true,
+        submenu: true
+      },
+      pages: {
+        showcase: false
+      }
+    },
+
+    children: [
+      {
+        path: '',
+        component: () => import('pages/0-starting/6-rtl/'),
+        meta: {
+          status: 0
+        }
+      }
+    ]
+  },
 
   {
     path: '/style/color',
