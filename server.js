b6b52bdd9d58ad05ccf882ca3ab5dd4e5db3a226
Rodrigo Vieira <rodrigo@vieira.com>
2018-06-26 17:18:09 -0300

    Procfile

---
+++ server.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-path-concat */
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(history())
+app.use(serveStatic(__dirname + '/dist/spa-mat'))
+app.listen(port)
